8f0ce1123dcdc84ce78a70ea6bfc36409fbd9eab
Nahi-Terefe <nahi.terefe@gmail.com>
2021-10-06 05:35:14 +0300

    fix betty issue

--- driver.c
+++ driver.c
@@ -8,28 +8,27 @@
  * Return: a pointer
  * structype selector - Struct
  */
-
 int (*driver(char *format))(char *format, va_list)
 {
-    int i;
+	int i;
 
-    structype selector[] = {
-        {"%c", printc},
-        {"%s", printstr},
-        {"%d", printint},
-        {"%i", printint},
-        {"%%", printpercent},
-        {"%x", printhex},
-        {"%X", printHEX},
-        {"%o", printocta},
-        {NULL, NULL}};
+	structype selector[] = {
+		{"%c", printc},
+		{"%s", printstr},
+		{"%d", printint},
+		{"%i", printint},
+		{"%%", printpercent},
+		{"%x", printhex},
+		{"%X", printHEX},
+		{"%o", printocta},
+		{NULL, NULL}};
 
-    if (format[1] == ' ' || format[1] == '\0')
-        return (NULL);
-    for (i = 0; selector[i].q; i++)
-    {
-        if (format[1] == selector[i].q[1])
-            return (selector[i].u);
-    }
-    return (NULL);
+	if (format[1] == ' ' || format[1] == '\0')
+		return (NULL);
+	for (i = 0; selector[i].q; i++)
+	{
+		if (format[1] == selector[i].q[1])
+			return (selector[i].u);
+	}
+	return (NULL);
 }
